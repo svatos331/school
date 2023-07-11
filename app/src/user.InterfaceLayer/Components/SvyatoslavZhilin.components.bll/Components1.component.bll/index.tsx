aa36376c92ae4ba1c1f37d4252546347fdee65f2
svyatoslaczhilin <s.zhilin@profsoft.pro>
2023-07-11 20:00:17 +0300

    fix after code review

--- app/src/user.InterfaceLayer/Components/SvyatoslavZhilin.components.bll/Components1.component.bll/index.tsx
+++ app/src/user.InterfaceLayer/Components/SvyatoslavZhilin.components.bll/Components1.component.bll/index.tsx
@@ -1,13 +1,6 @@
 import React from "react";
 
-import { useGetTodoQuery } from "../../../../business.InterfaceLayer/store/shared/entities/svyatoslavZhilin.entities/todo.entity/redux/api";
-
 const Component1 = () => {
-	const { data } = useGetTodoQuery({ authToken: "", params: { id: "1" } });
-
-	// eslint-disable-next-line no-console
-	console.log(321, data);
-
 	return (
 		<div>
 			<div>11</div>
