27021ae84f334ebcf6189831a573ae44cb5b66c7
svyatoslaczhilin <s.zhilin@profsoft.pro>
2023-07-11 16:14:29 +0300

    init

---
+++ app/src/user.InterfaceLayer/Pages/SvyatoslavZhilin.pages/Main.page/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+// import { Component1 } from "test-lib";
+
+const MainPage: FC = () => {
+	// eslint-disable-next-line no-console
+	// console.log(Component1);
+
+	return <div>SvyatoslavZhilinMainPage</div>;
+};
+
+export default MainPage;
+
